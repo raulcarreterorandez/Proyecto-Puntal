eaa48e5c50d61b083effbd874c9bdb068bc6b4f2
Raul Carretero Randez <raulcarreterorandezrandez@gmail.com>
2023-01-17 14:21:12 +0100

    17/01/23 Actualizacion BD

--- Documentacion/BD/pruebas_puntal.sql
+++ Documentacion/BD/pruebas_puntal.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Servidor: database:3306
--- Tiempo de generación: 16-01-2023 a las 08:21:04
+-- Servidor: mysql
+-- Tiempo de generación: 17-01-2023 a las 13:20:11
 -- Versión del servidor: 8.0.31
--- Versión de PHP: 8.0.25
+-- Versión de PHP: 8.0.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `puntalBD`
+-- Base de datos: `pruebas_puntal`
 --
 
 -- --------------------------------------------------------
@@ -47,7 +47,7 @@ INSERT INTO `base` (`idPlaza`, `fechaEntrada`, `fechaSalida`) VALUES
 --
 
 CREATE TABLE `cliente` (
-  `id` tinyint NOT NULL,
+  `numDocumento` varchar(20) NOT NULL,
   `nombre` varchar(20) NOT NULL,
   `apellidos` varchar(50) NOT NULL,
   `email` varchar(40) NOT NULL,
@@ -61,9 +61,9 @@ CREATE TABLE `cliente` (
 -- Volcado de datos para la tabla `cliente`
 --
 
-INSERT INTO `cliente` (`id`, `nombre`, `apellidos`, `email`, `direccion`, `tipoDocumento`, `Observaciones`, `visto`) VALUES
-(1, 'Luis', 'Corberán Izquierdo', 'luiscorberan@luis.com', 'Av/ Ausias March , 22', 'DNI', 'Es la primera inserción que hacemos. Es una prueba.', 0),
-(3, 'Raúl', 'Carretero García', 'raulcarretero@raul.com', 'C/ Islas Canarias 5, 12', 'DNI', 'Hola me llamo raúl. Esto es una prueba.', 0);
+INSERT INTO `cliente` (`numDocumento`, `nombre`, `apellidos`, `email`, `direccion`, `tipoDocumento`, `Observaciones`, `visto`) VALUES
+('1', 'Luis', 'Corberán Izquierdo', 'luiscorberan@luis.com', 'Av/ Ausias March , 22', 'DNI', 'Es la primera inserción que hacemos. Es una prueba.', 0),
+('3', 'Raúl', 'Carretero García', 'raulcarretero@raul.com', 'C/ Islas Canarias 5, 12', 'DNI', 'Hola me llamo raúl. Esto es una prueba.', 0);
 
 -- --------------------------------------------------------
 
@@ -72,13 +72,13 @@ INSERT INTO `cliente` (`id`, `nombre`, `apellidos`, `email`, `direccion`, `tipoD
 --
 
 CREATE TABLE `embarcacion` (
-  `id` tinyint NOT NULL,
+  `matricula` varchar(20) NOT NULL,
   `nombre` varchar(40) NOT NULL,
   `eslora` int NOT NULL,
   `manga` int NOT NULL,
   `calado` int NOT NULL,
   `propulsion` varchar(30) NOT NULL,
-  `id_cliente` tinyint NOT NULL,
+  `id_cliente` varchar(20) NOT NULL,
   `id_plaza` tinyint NOT NULL,
   `visto` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -87,8 +87,8 @@ CREATE TABLE `embarcacion` (
 -- Volcado de datos para la tabla `embarcacion`
 --
 
-INSERT INTO `embarcacion` (`id`, `nombre`, `eslora`, `manga`, `calado`, `propulsion`, `id_cliente`, `id_plaza`, `visto`) VALUES
-(1, 'María la Portuguesa', 20, 8, 5, 'Vela / Motor', 1, 1, 0);
+INSERT INTO `embarcacion` (`matricula`, `nombre`, `eslora`, `manga`, `calado`, `propulsion`, `id_cliente`, `id_plaza`, `visto`) VALUES
+('1', 'María la Portuguesa', 20, 8, 5, 'Vela / Motor', '1', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -123,7 +123,7 @@ INSERT INTO `instalacion` (`id`, `codigo`, `nombrePuerto`, `descripcion`, `estad
 CREATE TABLE `instalacionesUsuarios` (
   `id` tinyint NOT NULL,
   `idInstalacion` tinyint NOT NULL,
-  `idUsuario` tinyint NOT NULL
+  `idUsuario` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -137,8 +137,8 @@ CREATE TABLE `mensajes` (
   `texto` varchar(200) NOT NULL,
   `fecha/hora` timestamp NOT NULL,
   `leido` tinyint(1) NOT NULL,
-  `idUsuarioOrigen` tinyint NOT NULL,
-  `idUsuarioDestino` tinyint NOT NULL
+  `idUsuarioOrigen` varchar(50) NOT NULL,
+  `idUsuarioDestino` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -191,7 +191,7 @@ INSERT INTO `plaza` (`id`, `disponible`, `visto`, `puertoOrigen`, `puertoDestino
 
 CREATE TABLE `telefono` (
   `numero` int NOT NULL,
-  `idCliente` tinyint NOT NULL
+  `idCliente` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
@@ -199,7 +199,7 @@ CREATE TABLE `telefono` (
 --
 
 INSERT INTO `telefono` (`numero`, `idCliente`) VALUES
-(1, 1);
+(1, '1');
 
 -- --------------------------------------------------------
 
@@ -227,7 +227,7 @@ INSERT INTO `transito` (`idPlaza`, `fechaEntrada`, `fechaSalida`) VALUES
 --
 
 CREATE TABLE `tripulante` (
-  `id` tinyint NOT NULL,
+  `numDocumento` varchar(20) NOT NULL,
   `nombre` varchar(30) NOT NULL,
   `apellidos` varchar(40) NOT NULL,
   `nacionalidad` varchar(30) NOT NULL,
@@ -238,7 +238,7 @@ CREATE TABLE `tripulante` (
   `fechaExpedicionDocumento` date NOT NULL,
   `fechaCaducidadDocumento` date NOT NULL,
   `id_plaza` tinyint NOT NULL,
-  `id_embarcacion` tinyint NOT NULL,
+  `id_embarcacion` varchar(20) NOT NULL,
   `visto` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -246,8 +246,8 @@ CREATE TABLE `tripulante` (
 -- Volcado de datos para la tabla `tripulante`
 --
 
-INSERT INTO `tripulante` (`id`, `nombre`, `apellidos`, `nacionalidad`, `fechaNacimiento`, `lugarNacimiento`, `paisNacimiento`, `tipoDocumento`, `fechaExpedicionDocumento`, `fechaCaducidadDocumento`, `id_plaza`, `id_embarcacion`, `visto`) VALUES
-(1, 'Juan', 'García García', 'española', '2023-01-11', 'Valencia', 'España', 'DNI', '2023-01-11', '2023-01-12', 1, 1, 0);
+INSERT INTO `tripulante` (`numDocumento`, `nombre`, `apellidos`, `nacionalidad`, `fechaNacimiento`, `lugarNacimiento`, `paisNacimiento`, `tipoDocumento`, `fechaExpedicionDocumento`, `fechaCaducidadDocumento`, `id_plaza`, `id_embarcacion`, `visto`) VALUES
+('1', 'Juan', 'García García', 'española', '2023-01-11', 'Valencia', 'España', 'DNI', '2023-01-11', '2023-01-12', 1, '1', 0);
 
 -- --------------------------------------------------------
 
@@ -256,25 +256,15 @@ INSERT INTO `tripulante` (`id`, `nombre`, `apellidos`, `nacionalidad`, `fechaNac
 --
 
 CREATE TABLE `usuario` (
-  `id` tinyint NOT NULL,
   `nombreUsuario` varchar(20) NOT NULL,
-  `contraseña` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
   `nombreCompleto` varchar(50) NOT NULL,
-  `email` varchar(30) NOT NULL,
+  `email` varchar(50) NOT NULL,
   `habilitado` tinyint(1) NOT NULL,
   `perfil` varchar(20) NOT NULL,
-  `idioma` varchar(15) NOT NULL,
+  `idioma` varchar(20) NOT NULL,
   `visto` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Volcado de datos para la tabla `usuario`
---
-
-INSERT INTO `usuario` (`id`, `nombreUsuario`, `contraseña`, `nombreCompleto`, `email`, `habilitado`, `perfil`, `idioma`, `visto`) VALUES
-(2, 'LUISyALF', 'luisluis', 'Luis Corberán Izquierdo', 'luis@luis.com', 1, 'GuardaMuelles', 'Español', 0),
-(3, 'RAUWCarretero', 'raulraul', 'Raúl Carretero García', 'raul@raul.com', 1, 'GuardaMuelles', 'Español', 0),
-(4, 'JuanGuarnizo', 'juanjuan', 'Juan Saéz Guarnizo', 'juan@juan.com', 1, 'Administrativo', 'Inglés', 0);
 
 --
 -- Índices para tablas volcadas
@@ -290,13 +280,13 @@ ALTER TABLE `base`
 -- Indices de la tabla `cliente`
 --
 ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`numDocumento`);
 
 --
 -- Indices de la tabla `embarcacion`
 --
 ALTER TABLE `embarcacion`
-  ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`matricula`),
   ADD KEY `R7` (`id_plaza`),
   ADD KEY `R8` (`id_cliente`);
 
@@ -353,7 +343,7 @@ ALTER TABLE `transito`
 -- Indices de la tabla `tripulante`
 --
 ALTER TABLE `tripulante`
-  ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`numDocumento`),
   ADD KEY `R5` (`id_embarcacion`),
   ADD KEY `R6` (`id_plaza`);
 
@@ -361,7 +351,7 @@ ALTER TABLE `tripulante`
 -- Indices de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`email`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -372,18 +362,6 @@ ALTER TABLE `usuario`
 --
 ALTER TABLE `base`
   MODIFY `idPlaza` tinyint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT de la tabla `cliente`
---
-ALTER TABLE `cliente`
-  MODIFY `id` tinyint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT de la tabla `embarcacion`
---
-ALTER TABLE `embarcacion`
-  MODIFY `id` tinyint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `instalacion`
@@ -416,28 +394,10 @@ ALTER TABLE `plaza`
   MODIFY `id` tinyint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT de la tabla `telefono`
---
-ALTER TABLE `telefono`
-  MODIFY `numero` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
 -- AUTO_INCREMENT de la tabla `transito`
 --
 ALTER TABLE `transito`
   MODIFY `idPlaza` tinyint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT de la tabla `tripulante`
---
-ALTER TABLE `tripulante`
-  MODIFY `id` tinyint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `id` tinyint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restricciones para tablas volcadas
@@ -453,22 +413,22 @@ ALTER TABLE `base`
 -- Filtros para la tabla `embarcacion`
 --
 ALTER TABLE `embarcacion`
-  ADD CONSTRAINT `R7` FOREIGN KEY (`id_plaza`) REFERENCES `plaza` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `R8` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `Embarcacion-Cliente` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`numDocumento`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `Embarcacion-Plaza` FOREIGN KEY (`id_plaza`) REFERENCES `plaza` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `instalacionesUsuarios`
 --
 ALTER TABLE `instalacionesUsuarios`
-  ADD CONSTRAINT `RelacionInstalacion` FOREIGN KEY (`idInstalacion`) REFERENCES `instalacion` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `RelacionUsuario` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `Relacion-Instalacion` FOREIGN KEY (`idInstalacion`) REFERENCES `instalacion` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `Relacion-Usuario` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`email`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `mensajes`
 --
 ALTER TABLE `mensajes`
-  ADD CONSTRAINT `R1` FOREIGN KEY (`idUsuarioDestino`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `R2` FOREIGN KEY (`idUsuarioOrigen`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `Usuario-Destino` FOREIGN KEY (`idUsuarioDestino`) REFERENCES `usuario` (`email`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `Usuario-Origen` FOREIGN KEY (`idUsuarioOrigen`) REFERENCES `usuario` (`email`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `muelle`
@@ -486,7 +446,7 @@ ALTER TABLE `plaza`
 -- Filtros para la tabla `telefono`
 --
 ALTER TABLE `telefono`
-  ADD CONSTRAINT `R9` FOREIGN KEY (`idCliente`) REFERENCES `cliente` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `Telefono-Cliente` FOREIGN KEY (`idCliente`) REFERENCES `cliente` (`numDocumento`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `transito`
@@ -498,8 +458,8 @@ ALTER TABLE `transito`
 -- Filtros para la tabla `tripulante`
 --
 ALTER TABLE `tripulante`
-  ADD CONSTRAINT `R5` FOREIGN KEY (`id_embarcacion`) REFERENCES `embarcacion` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `R6` FOREIGN KEY (`id_plaza`) REFERENCES `plaza` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `Tripulante-Embarcacion` FOREIGN KEY (`id_embarcacion`) REFERENCES `embarcacion` (`matricula`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `Tripulante-PlazaTransito` FOREIGN KEY (`id_plaza`) REFERENCES `transito` (`idPlaza`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
