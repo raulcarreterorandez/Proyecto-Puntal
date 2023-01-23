261cbf93e53b8b0e58796ce154c5888a5a1dab0e
LUISyALF <luiscorberan@gmail.com>
2023-01-23 17:41:15 +0100

    23/01/23 Login casi terminado. Falta conexión.

--- Angular12JwtAuth/src/app/app.module.ts
+++ Angular12JwtAuth/src/app/app.module.ts
@@ -5,21 +5,22 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardModule } from './dashboard/dashboard.module'; /* Aquí nuestro home y login */
+import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { BoardAdminComponent } from './board-admin/board-admin.component';
 import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
 import { BoardUserComponent } from './board-user/board-user.component';
 
-/* import { authInterceptorProviders } from './_helpers/auth.interceptor'; */
-
-import { DataTablesModule } from "angular-datatables";
+import { authInterceptorProviders } from './_helpers/auth.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
     RegisterComponent,
+    HomeComponent,
     ProfileComponent,
     BoardAdminComponent,
     BoardModeratorComponent,
@@ -28,10 +29,10 @@ import { DataTablesModule } from "angular-datatables";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DashboardModule,
-    DataTablesModule
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [/* authInterceptorProviders */],
+  providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
