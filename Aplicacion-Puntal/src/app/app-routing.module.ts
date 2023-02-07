1b41ac1cfaa4837ead0cb6b37d9721f8096221a2
LUISyALF <luiscorberan@gmail.com>
2023-02-07 18:33:33 +0100

    07/02/23. Cambios para poder hacer un pull

--- Aplicacion-Puntal/src/app/app-routing.module.ts
+++ Aplicacion-Puntal/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './dashboard/home/home.component';
-import { LoginComponent } from './dashboard/login/login.component';
-import { FormularioAltaInstalacionComponent } from './instalaciones/formulario-alta-instalacion/formulario-alta-instalacion.component';
+import { LoginComponent } from './dashboard/login/login.component';/* 
+import { FormularioAltaInstalacionComponent } from './instalaciones/formulario-alta-instalacion/formulario-alta-instalacion.component'; */
+import { VistaDetalladaInstalacionComponent } from './instalaciones/vista-detallada-instalacion/vista-detallada-instalacion.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'añadirInst', component: FormularioAltaInstalacionComponent },
+  { path: 'login', component: LoginComponent },/* 
+  { path: 'añadirInst', component: FormularioAltaInstalacionComponent }, */
+  { path: 'vistaDetalleInst', component: VistaDetalladaInstalacionComponent },
 ];
 
 @NgModule({
