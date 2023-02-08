71bd759a8ecb3a668c740d74f503f8be5a750c3e
LUISyALF <luiscorberan@gmail.com>
2023-02-08 10:38:14 +0100

    08/02/23 Reorganizacion Angular

--- Aplicacion-Puntal-Frontend/src/app/app.module.ts
+++ Aplicacion-Puntal-Frontend/src/app/app.module.ts
@@ -6,24 +6,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardModule } from './dashboard/dashboard.module'; /* Aquí nuestro home y login */
-import { RegisterComponent } from './register/register.component';
-import { ProfileComponent } from './profile/profile.component';
-import { BoardAdminComponent } from './board-admin/board-admin.component';
-import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
-import { BoardUserComponent } from './board-user/board-user.component';
+
 
 /* import { authInterceptorProviders } from './_helpers/auth.interceptor'; */
 
 import { DataTablesModule } from "angular-datatables";
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent,
-    ProfileComponent,
-    BoardAdminComponent,
-    BoardModeratorComponent,
-    BoardUserComponent
+    LoginComponent,
+    HomeComponent,
+
   ],
   imports: [
     BrowserModule,
--- Aplicacion-Puntal-Frontend/src/app/dashboard/home/home.component.spec.ts
+++ Aplicacion-Puntal-Frontend/src/app/dashboard/home/home.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { BoardAdminComponent } from './board-admin.component';
+import { HomeComponent } from './home.component';
 
-describe('BoardAdminComponent', () => {
-  let component: BoardAdminComponent;
-  let fixture: ComponentFixture<BoardAdminComponent>;
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BoardAdminComponent ]
+      declarations: [ HomeComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(BoardAdminComponent);
+    fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
