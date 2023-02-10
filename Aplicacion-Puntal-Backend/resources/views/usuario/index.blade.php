9780feb0bf304faacd465c7467259c275ac9a64c
Raul Carretero Randez <raulcarreterorandezrandez@gmail.com>
2023-02-10 09:09:53 +0100

    10/02/23 Comentarios Ayuda

--- Aplicacion-Puntal-Backend/resources/views/usuario/index.blade.php
+++ Aplicacion-Puntal-Backend/resources/views/usuario/index.blade.php
@@ -76,7 +76,10 @@
                                             <td>
                                                     <a class="btn btn-sm btn-primary " href="{{ route('usuarios.show',$usuario->email) }}" data-bs-toggle="tooltip"> <i class="bi bi-eye"></i> </a>
                                                     <a class="btn btn-sm btn-success" href="{{ route('usuarios.edit',$usuario->email) }}"><i class="bi bi-pencil"></i> </a>
-                                                    <a class="btn btn-sm btn-danger " href="{{ route('usuarios.confirm',$usuario->email) }}"><i class="bi bi-trash3-fill"></i> </a>
+
+                                                    @if ($usuario->habilitado === 1)
+                                                        <a class="btn btn-sm btn-danger " href="{{ route('usuarios.confirm',$usuario->email) }}"><i class="bi bi-trash3-fill"></i> </a>
+                                                    @endif
                                             </td>
                                         </tr>
                                     @endforeach
