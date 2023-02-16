e29cb264c0b129c9c006a2f6881b38f649b0eb3e
LUISyALF <luiscorberan@gmail.com>
2023-02-16 13:54:02 +0100

    16/02/23 Cambios en vistas muelles y en plazas.Luis

--- Aplicacion-Puntal-Frontend/src/app/plazas/plaza.ts
+++ Aplicacion-Puntal-Frontend/src/app/plazas/plaza.ts
@@ -1,4 +1,6 @@
-export interface Plaza {
+
+
+/* export interface Plaza {  ANTIGUO
 
     id?: any;
     disponible?: boolean;
@@ -7,4 +9,25 @@ export interface Plaza {
     puertoDestino?: string;
     anyo?: any;
     idMuelle?: any;
+    instalacion?: any; //Traeremos una colección con las propiedades de las instalaciones. Mirar como traemos la info. en el Index() de API/MuelleController del backend. 
+    plazas?:any; //Traeremos una colección con las propiedades de las plazas. Mirar como traemos la info. en el Index() de API/MuelleController del backend. 
+    tipo?:any
+} */
+
+
+export interface Plaza { //NUEVO
+
+    id?: any;
+    disponible?: boolean;
+    visto?: boolean;
+    puertoOrigen?: string;
+    puertoDestino?: string;
+    anyo?: any;
+    idMuelle?: any;
+    bases?: any;
+    transitos?: any;
+    instalacion?: any; //Traeremos una colección con las propiedades de las instalaciones. Mirar como traemos la info. en el Index() de API/MuelleController del backend. 
+    plazas?:any; //Traeremos una colección con las propiedades de las plazas. Mirar como traemos la info. en el Index() de API/MuelleController del backend. 
+    tipo?:any;
+    tripulantes?:any;
 }
