562fcb3a87b504ed60b428afd353688eb9cd2c69
Raul Carretero Randez <raulcarreterorandezrandez@gmail.com>
2023-02-23 09:19:50 +0100

    23/02/23 Modificaciones Detalles Funcionales

--- Aplicacion-Puntal-Backend/app/Http/Controllers/HomeController.php
+++ Aplicacion-Puntal-Backend/app/Http/Controllers/HomeController.php
@@ -5,12 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller {
-    
+
     public function __construct() { // Copia Instalación.
-            
-        $this->middleware('guarda-muelle'); // Desde Guarda-muelles hacia arriba, pasando por Gerencia y Xunta acceden a todo. 
+
+        $this->middleware('guarda-muelle'); // Desde Guarda-muelles hacia arriba, pasando por Gerencia y Xunta acceden a todo.
     }
-    
+
     public function index()
     {
         switch ( auth()->user()->perfil ) {
--- Aplicacion-Puntal-Backend/app/Http/Middleware/GuardaMuellesMiddleware.php
+++ Aplicacion-Puntal-Backend/app/Http/Middleware/GuardaMuellesMiddleware.php
@@ -10,18 +10,18 @@ class GuardaMuellesMiddleware {
     public function handle(Request $request, Closure $next)
     {
         if (Auth::check()) {    //si está autentificado
-           /*  if (auth()->user()->perfil == "GUARDA-MUELLES") { 
+           /*  if (auth()->user()->perfil == "GUARDA-MUELLES") {
 
             return $next($request);    //significa continua
             } */
 
-            if (auth()->user()->perfil == "GUARDA-MUELLES" ||                
+            if (auth()->user()->perfil == "GUARDA-MUELLES" ||
                 auth()->user()->perfil == "GERENCIA-PUERTO" ||
-                auth()->user()->perfil == "XUNTA-GALICIA") { 
+                auth()->user()->perfil == "XUNTA-GALICIA") {
 
                 return $next($request);    //significa continua
             }
         }
-        return redirect()->route('home')->with('access', 'Disabled access - Solo pueden acceder los usuarios con role GUARDA-MUELLES');
+        return redirect()->route('login')->with('access', 'Disabled access - Solo pueden acceder los usuarios con role GUARDA-MUELLES');
     }
 }
